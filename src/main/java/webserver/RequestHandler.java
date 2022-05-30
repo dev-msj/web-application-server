cd86b7253a20538f50f5e0ad925720caa89b9017
dev-msj <msj944@naver.com>
2022-05-31 02:09:49 +0900

    index.html 파일 응답 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.IOUtils;
+import util.RequestPathHandler;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,20 +22,55 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "My First Java Web Study".getBytes();
-//            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            RequestPathHandler requestPathHandler = new RequestPathHandler();
+            String path = extractPath(in);
+
+            if (requestPathHandler.isExistPath(path)) {
+                String html = requestPathHandler.readData(path);
+
+                // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+                byte[] body = html.getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+
+                return;
+            }
+
+            byte[] body = "페이지를 찾을 수 없습니다!".getBytes();
+            response404Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
+    private String extractPath(InputStream in) throws IOException {
+        String request = IOUtils.readData(new BufferedReader(new InputStreamReader(in)), in.available());
+        System.out.println("request = " + request);
+        return request.length() == 0 ? "" : getPathString(request);
+    }
+
+    private String getPathString(String request) {
+        String path = request.split("\n")[0].split(" ")[1];
+        
+        return path.equals("/") ? "/index.html" : path;
+    }
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
