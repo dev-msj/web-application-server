e55374bd8b9672a41f14b900b136ea8eb8c32ac7
dev-msj <msj944@naver.com>
2022-05-31 01:48:13 +0900

    사용되지 않는 getPath 메서드 삭제

--- src/main/java/util/RequestPathHandler.java
+++ src/main/java/util/RequestPathHandler.java
@@ -8,23 +8,14 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class RequestPathHandler {
-    private final String pattern = "(http|https)://(.*?)/(.*)";
+    private final String defaultPath = "./webapp/";
 
-    public String getPath(final String uri) {
-        Matcher matcher = Pattern.compile(pattern).matcher(uri);
-        if (matcher.find()) {
-            return matcher.group(3);
-        }
-
-        return "";
+    public boolean isExistPath(final String path) {
+        return new File(defaultPath + path).exists();
     }
 
-    public boolean isExistPath(final String uri) {
-        return new File("./webapp/" + getPath(uri)).exists();
-    }
-
-    public String readData(final String uri) {
-        try(Stream<String> stream = Files.lines(Paths.get("./webapp/" + getPath(uri)))) {
+    public String readData(final String path) {
+        try(Stream<String> stream = Files.lines(Paths.get(defaultPath + path))) {
             return parseDataToString(stream);
         } catch (IOException e) {
             System.out.println(e.toString());
