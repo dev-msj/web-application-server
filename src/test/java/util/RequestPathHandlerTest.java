d808fa356d18e7c687b50d419e2da3b877d765d4
dev-msj <msj944@naver.com>
2022-05-31 02:19:36 +0900

    uri를 사용하지 않고 request path를 사용해 테스트하도록 변경

--- src/test/java/util/RequestPathHandlerTest.java
+++ src/test/java/util/RequestPathHandlerTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 public class RequestPathHandlerTest {
     private final RequestPathHandler handler = new RequestPathHandler();
-    private final String uri = "http://localhost:8080/index.html";
+    private final String path = "/index.html";
 
     @Test
     public void findFileTest() {
-        Assert.assertTrue(handler.isExistPath(uri));
+        Assert.assertTrue(handler.isExistPath(path));
     }
 
     @Test
     public void getFileData() {
-        Assert.assertNotEquals("IOException", handler.readData(uri));
+        Assert.assertNotEquals("IOException", handler.readData(path));
     }
 }
