587f568a733d886960e74f7eb477cab586090518
dev-msj <msj944@naver.com>
2022-05-31 00:19:02 +0900

    webapp 경로에서 path에 해당하는 파일을 로드 기능 구현

--- src/main/java/util/RequestPathHandler.java
+++ src/main/java/util/RequestPathHandler.java
@@ -1,7 +1,11 @@
 package util;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class RequestPathHandler {
     private final String pattern = "(http|https)://(.*?)/(.*)";
@@ -13,5 +17,26 @@ public class RequestPathHandler {
         }
 
         return "";
+    }
+
+    public boolean isExistPath(final String uri) {
+        return new File("./webapp/" + getPath(uri)).exists();
+    }
+
+    public String readData(final String uri) {
+        try(Stream<String> stream = Files.lines(Paths.get("./webapp/" + getPath(uri)))) {
+            return parseDataToString(stream);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+
+            return "IOException";
+        }
+    }
+
+    private String parseDataToString(final Stream<String> stream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stream.forEach(line -> stringBuilder.append(line).append("\n"));
+
+        return stringBuilder.toString();
     }
 }
--- src/test/java/util/RequestPathHandlerTest.java
+++ src/test/java/util/RequestPathHandlerTest.java
@@ -6,13 +6,24 @@ import org.junit.Test;
 
 public class RequestPathHandlerTest {
     private final RequestPathHandler handler = new RequestPathHandler();
+    private final String uri = "http://localhost:8080/index.html";
 
     @Test
     public void findPathTest() {
         String http = "http://test.co.kr:8080/my/test/path.html";
-        Assert.assertThat(handler.getPath(http), CoreMatchers.is("my/test/path.html"));
+        Assert.assertEquals("my/test/path.html", handler.getPath(http));
 
         String https = "https://aha.com/take/on/me.jsp";
-        Assert.assertThat(handler.getPath(https), CoreMatchers.is("take/on/me.jsp"));
+        Assert.assertEquals("take/on/me.jsp", handler.getPath(https));
+    }
+
+    @Test
+    public void findFileTest() {
+        Assert.assertTrue(handler.isExistPath(uri));
+    }
+
+    @Test
+    public void getFileData() {
+        Assert.assertNotEquals("IOException", handler.readData(uri));
     }
 }
