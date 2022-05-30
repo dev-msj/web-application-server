dc54f6b1ee0dbbc29fb3462d736fb28d13c72373
dev-msj <msj944@naver.com>
2022-05-31 02:17:15 +0900

    사용되지 않는 테스트 코드 삭제

--- src/test/java/util/RequestPathHandlerTest.java
+++ src/test/java/util/RequestPathHandlerTest.java
@@ -1,21 +1,11 @@
 package util;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RequestPathHandlerTest {
     private final RequestPathHandler handler = new RequestPathHandler();
     private final String uri = "http://localhost:8080/index.html";
-
-    @Test
-    public void findPathTest() {
-        String http = "http://test.co.kr:8080/my/test/path.html";
-        Assert.assertEquals("my/test/path.html", handler.getPath(http));
-
-        String https = "https://aha.com/take/on/me.jsp";
-        Assert.assertEquals("take/on/me.jsp", handler.getPath(https));
-    }
 
     @Test
     public void findFileTest() {
